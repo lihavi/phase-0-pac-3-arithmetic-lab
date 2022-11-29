4c3bd1d39d656ac7a0081b770e4a8d8e50ec71e8
vivianne <viviannjoroge91@gmail.com>
2022-11-29 09:57:04 +0300

    completed assignment

--- index.js
+++ index.js
@@ -10,4 +10,22 @@ return a*b;
   function divide(a,b) {
 return a/b
   }
-  
+  function increment(n){
+    var o = n
+    n++
+    return n 
+  }
+function decrement(n){
+  var p = n
+  n--
+  return n
+}
+function makeInt(n){
+  return n
+}
+function makeInt(string){
+  return parseInt(string,10)
+}
+function preserveDecimal(n){
+  return parseFloat(n)
+}
